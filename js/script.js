72163de9dc6c1572415a76e2aeb0ecfdc76cfc94
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-02-07 18:07:54 +0000

    Added implementation to display the previous search history on a web page as buttons. The search history data is stored in an array and retrieved from local storage. The function iterates through each item in the search history array and creates a button for each, adding it to the historyList element. The button displays the searched item as its text and has a data-search attribute set to the searched item. The historyList element listens to clicks on the buttons, and when one is clicked, it calls the GeolocationCoordinates function with the data-search value as an argument.

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  const currentDay = moment().format("DD/MMM/YYYY, HH:mm");
+  $("#current-day").html(currentDay);
+
+  let unit = "metric";
+  let apiKey = "baad171896e0c3b36f831a6990f30812";
+	let searchHistory = [];
+	historyList = $('#history');
+
+
+
+	// The purpose of this function is to display a list of previously
+	// searched items (stored in a searchHistory array) on a web page as buttons.
+  function displaySearchHistory() {
+    let storedSearchHistory = localStorage.getItem("searchHistory");
+		if(storedSearchHistory){
+			searchHistory = JSON.parse(storedSearchHistory);
+		}
+		historyList.html('');
+		// render search history buttons on the page
+
+		//for loop to iterate through each item in the searchHistory array.
+		for(let i = 0; i < searchHistory.length; i++){
+			// create a new button element
+			let historyBtn = $('<button>');
+			historyBtn.attr('type', 'button');
+			historyBtn.attr('data-search', searchHistory[i]);
+			historyBtn.addClass('history-btn btn btn-primary').text(searchHistory[i]).css('margin-right', '10px');
+			// appends the button to the historyList element.
+			historyList.append(historyBtn);
+		}
+		historyList.on('click', '.history-btn', function(){
+			let searchValue = $(this).attr('data-search');
+			GeolocationCoordinates(searchValue);
+		})
+  }
+
+
+
+
+
+
+});
